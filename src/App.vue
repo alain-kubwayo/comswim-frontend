d141bb4fb11a91bd6dc79a61bd024d9471ca6647
Alain Kubwayo <ahkubwayo@gmail.com>
2024-02-10 13:21:50 +0200

    chore: setup routing

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 </script>
 
 <template>
-  <div>
+  <div id="nav">
     <h1>Comswim</h1>
+    <router-link :to="{ name: 'Login' }">Login</router-link>
+    <router-link :to="{ name: 'Register' }">Register</router-link>
   </div>
+  <router-view />
 </template>
 
 <style scoped>
