3c063bec6ecf2c7b710cc2e85113f54fa101ddd7
Alain Kubwayo <ahkubwayo@gmail.com>
2024-02-10 11:23:39 +0200

    chore: project setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1>App</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+  
+</style>
