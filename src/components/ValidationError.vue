c9336b6f9c7585794feff7ef4a7b8eb4be8ac62b
Alain Kubwayo <ahkubwayo@gmail.com>
2024-03-15 10:55:25 +0200

    refactor: remove unused code from ValidationError.vue

--- src/components/ValidationError.vue
+++ src/components/ValidationError.vue
@@ -1,19 +1,3 @@
-<!-- <script setup>
-    import { defineProps } from 'vue';
-    const props = defineProps(["errors", "field"]);
-</script>
-
-<template>
-    <ul
-        v-if="props?.errors?.value?.[props.field]?.length"
-        class="flex flex-col gap-1 text-red-600"
-    >
-        <li v-for="message in props.errors.value[props.field]" :key="message">
-            {{  message }}
-        </li>
-    </ul>
-</template> -->
-
 <template>
   <ul
     v-if="errors && errors[field] && errors[field].length"
